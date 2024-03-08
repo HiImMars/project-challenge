60ccced016fc0d054c938ebc0d5765820620ac9a
HiImMars <fedorenkorostyslav@gmail.com>
2024-03-08 02:13:37 +0200

    dropdown

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
 import StarRating from "./Rating";
 import { useGetOffersQuery } from "./redux/offersApi";
 import { FaRegHeart } from "react-icons/fa";
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 
 function App() {
   const { data, isError, isLoading } = useGetOffersQuery();
+
+  const dropDownCategories = [
+    {
+      category: "Services",
+      image: "/images/categories/img-1.jpg",
+    },
+    {
+      category: "Real Estate",
+      image: "/images/categories/img-2.jpg",
+    },
+    {
+      category: "For Rent",
+      image: "/images/categories/img-3.jpg",
+    },
+    {
+      category: "Items For Sale",
+      image: "/images/categories/img-4.jpg",
+    },
+    {
+      category: "Jobs",
+      image: "/images/categories/img-5.png",
+    },
+  ];
 
   return (
     <>
@@ -27,8 +51,6 @@ function App() {
                 <div className="px-3 py-2 flex flex-col gap-2">
                   <div className="flex justify-between">
                     <p className="text-[13px]">{category.title}</p>
-
-                    {/* <p>{rating}</p> */}
                     <StarRating rate={rating} />
                   </div>
 
@@ -50,6 +72,22 @@ function App() {
               </li>
             );
           })}
+        </ul>
+      </div>
+      <div className="min-w-[350px] max-w-[540px] md:max-w-[1200px] xl:max-w-[1192px] mx-auto md:p-6 p-4">
+        <ul className="flex flex-col after:content-[''] after:w-[1px] after:h-full after:text-blue-100">
+          {dropDownCategories.map(({ category, image }) => (
+            <li
+              key={category}
+              className="flex justify-between items-center w-[295px] px-2 py-3 hover:bg-blue-100 transition"
+            >
+              <div className="flex gap-4 justify-center items-center">
+                <img src={image} alt="category" />
+                <p>{category}</p>
+              </div>
+              <MdOutlineKeyboardArrowRight className="w-[20px] h-[20px]" />
+            </li>
+          ))}
         </ul>
       </div>
     </>
